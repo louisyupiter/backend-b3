3d2b008133ccbf6349f6321a293a290803d975cf
consolatio <consolatio1234@gmail.com>
2021-12-14 10:51:33 +0700

    update deploy

--- app.js
+++ app.js
@@ -15,33 +15,9 @@ mongooseConnect();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/images", express.static(path.join("images")));
-const Post = require("./models/Post");
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-  );
-  next();
-});
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-app.get("/api", async (req, res, next) => {
-  try {
-    const result = await Post.find({});
-    if (result.length === 0) {
-      throw { name: "NOT_FOUND" };
-    } else {
-      res.status(200).json({ message: "success", result: result });
-    }
-  } catch (error) {
-    next(error);
-  }
+app.get("/", (_req, res) => {
+  res.header('Content-type', 'text/html');
+    return res.end('<h1>Hello, Secure World!</h1>');
 });
 app.use("/api/posts", postsRoutes);
 
