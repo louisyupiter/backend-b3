8ec5d247cfb21bf5d456b3e590915025c12f7d49
consolatio <consolatio1234@gmail.com>
2021-12-14 09:22:39 +0700

    update deploy

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const express = require("express");
 const mongooseConnect = require("./configs/db");
 const errorHandler = require("./middlewares/errorHandler");
 const app = express();
+const cors = require('cors');
 // const port = 3000;
 
 const postsRoutes = require('./routes/posts')
+
+app.use(cors());
 
 mongooseConnect();
 
@@ -20,3 +23,4 @@ app.use(errorHandler);
 // app.listen(port, () => {
 //   console.log(`Example app listening at http://localhost:${port}`);
 // });
+module.exports = app;
