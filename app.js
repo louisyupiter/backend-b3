8af61d72074d43839cbd3bdad10ee689fcd538b2
consolatio <consolatio1234@gmail.com>
2021-12-14 09:32:02 +0700

    update deploy

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const path = require('path');
+const path = require("path");
 const express = require("express");
 const mongooseConnect = require("./configs/db");
 const errorHandler = require("./middlewares/errorHandler");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 // const port = 3000;
 
-const postsRoutes = require('./routes/posts')
+const postsRoutes = require("./routes/posts");
 
 app.use(cors());
 
@@ -14,8 +14,10 @@ mongooseConnect();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/images', express.static(path.join('images')));
-
+app.use("/images", express.static(path.join("images")));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 app.use("/api/posts", postsRoutes);
 
 app.use(errorHandler);
