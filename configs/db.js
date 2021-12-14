4fa3a6981e5184c9857553e1578e334d525cb304
consolatio <consolatio1234@gmail.com>
2021-12-14 10:21:13 +0700

    update deploy

--- configs/db.js
+++ configs/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 main = async () => {
   const dburl = `mongodb://localhost/multertest`;
   try {
-    await mongoose.connect(dburl);
+    await mongoose.connect("mongodb://localhost/multertest");
     console.log(`Mongoose Connected!`);
   } catch (error) {
     console.log(`Mongoose FAILED!`);
