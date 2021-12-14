dfb0cf7c69403950ec7451e6eab4f919a2efdc35
consolatio <consolatio1234@gmail.com>
2021-12-14 09:33:51 +0700

    update deploy

--- app.js
+++ app.js
@@ -15,6 +15,18 @@ mongooseConnect();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/images", express.static(path.join("images")));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
