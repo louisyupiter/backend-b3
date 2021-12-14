2d2eba04df2782a7f4798c3a40d32f94ff9bd3f0
consolatio <consolatio1234@gmail.com>
2021-12-14 09:15:59 +0700

    update deploy

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const errorHandler = require("./middlewares/errorHandler");
 const app = express();
 // const port = 3000;
 
-const http = require('http');
-const port = process.env.PORT || 3000
-
-
 const postsRoutes = require('./routes/posts')
 
 mongooseConnect();
@@ -21,17 +17,6 @@ app.use("/api/posts", postsRoutes);
 
 app.use(errorHandler);
 
-app.get('/', (req,res)=>{
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>')
-})
-
 // app.listen(port, () => {
 //   console.log(`Example app listening at http://localhost:${port}`);
 // });
-
-const server = http.createServer(app);
-
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
-});
